d99d402ba2c6b6d87a28670acf4e899664887a9d
biktiko <btigran02@gmail.com>
2024-12-25 22:20:37 +0400

    Настройка Amazon S3 для хранения медиа-файлов 5

--- reservon/custom_storages.py
+++ reservon/custom_storages.py
@@ -1,11 +1,11 @@
-import logging
-from storages.backends.s3boto3 import S3Boto3Storage
+# import logging
+# from storages.backends.s3boto3 import S3Boto3Storage
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-class MediaStorage(S3Boto3Storage):
-    file_overwrite = False
+# class MediaStorage(S3Boto3Storage):
+#     file_overwrite = False
 
-    def save(self, name, content, max_length=None):
-        logger.info(f"MediaStorage.save() name={name}")
-        return super().save(name, content, max_length=max_length)
+#     def save(self, name, content, max_length=None):
+#         logger.info(f"MediaStorage.save() name={name}")
+#         return super().save(name, content, max_length=max_length)
