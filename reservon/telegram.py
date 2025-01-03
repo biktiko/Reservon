cf6714d23e926fdf16a924489b4225ba9eab40eb
biktiko <btigran02@gmail.com>
2025-01-03 20:46:59 +0400

    Try integrate Telegram sms and fix dublicate push 7

--- reservon/telegram.py
+++ reservon/telegram.py
@@ -8,23 +8,16 @@ INTERCONNECT_API_URL = "https://portal.interconnect.solutions/api/json.php"
 INTERCONNECT_API_KEY = "38e47e13-2733-4925-96ee-59752b05d152"
 
 def send_telegram_via_interconnect(phone_number: str, message: str, msg_id: int = 1) -> bool:
-    """
-    Отправляет сообщение через сервис InterConnect Solutions по Telegram.
-    phone_number: строка с номером вида '380971234567' (без '+', если того требует API).
-    message: строка, которую отправляем.
-    msg_id: ваш уникальный ID для запроса (просто число).
-    Возвращает True/False — успех или нет.
-    """
+
     payload = {
         "auth": INTERCONNECT_API_KEY,
         "data": [
             {
                 "type": "telegram",
-                "id": msg_id,
+                "id": 100500,
                 "phone": phone_number,
                 "message": message,
                 "telegram_lifetime": 172800,   # 2 дня
-                # "hook": "https://example.org/webhook/url.php",  # если нужно
             }
         ]
     }
