69dbc65b4c341cee549d152b6edbb679e12df18a
biktiko <btigran02@gmail.com>
2025-01-03 20:01:34 +0400

    Try integrate Telegram sms and fix dublicate push 5

--- reservon/telegram.py
+++ reservon/telegram.py
@@ -1,9 +1,10 @@
+# C:\Reservon\Reservon\reservon\telegram.py
 import requests
 import logging
 
 logger = logging.getLogger(__name__)
 
-INTERCONNECT_API_URL = "https://reservon.am/api/json.php"  # Подставьте реальный URL
+INTERCONNECT_API_URL = "https://portal.interconnect.solutions/api/json.php"
 INTERCONNECT_API_KEY = "38e47e13-2733-4925-96ee-59752b05d152"
 
 def send_telegram_via_interconnect(phone_number: str, message: str, msg_id: int = 1) -> bool:
